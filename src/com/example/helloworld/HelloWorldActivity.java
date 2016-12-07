4204558b3eae9dfc7f2769b043446125d37c560b
Administrator <Administrator@7A304-0033>
2016-12-07 11:00:57 +0800

    利用回调机制实现点击tabbar中不同的按钮触发不同的事件

--- src/com/example/helloworld/HelloWorldActivity.java
+++ src/com/example/helloworld/HelloWorldActivity.java
@@ -1,14 +1,73 @@
 package com.example.helloworld;
 
+import com.example.helloworld.fragments.MainTabbarFragment;
+import com.example.helloworld.fragments.MainTabbarFragment.OnTabSelectedListener;
+import com.example.helloworld.fragments.pages.FeedsListFragment;
+import com.example.helloworld.fragments.pages.MyProfileFragment;
+import com.example.helloworld.fragments.pages.NoteListFragment;
+import com.example.helloworld.fragments.pages.SearchPageFragment;
+
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 
 public class HelloWorldActivity extends Activity {
+
+	FeedsListFragment contentFeedList = new FeedsListFragment();
+	NoteListFragment contentNoteList = new NoteListFragment();
+	MyProfileFragment contentMyProfile = new MyProfileFragment();
+	SearchPageFragment contentSearchPage = new SearchPageFragment();
+	MainTabbarFragment tabbar = new MainTabbarFragment();
+	Fragment frags[];
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_helloworld);
+		frags = new Fragment[] { contentFeedList, contentNoteList, contentMyProfile, contentSearchPage };
+		tabbar = (MainTabbarFragment) getFragmentManager().findFragmentById(R.id.frag_tabbar);
+		tabbar.setOnTabSelectedListener(new OnTabSelectedListener() {
+			
+			@Override
+			public void onTabSelected(int index) {
+				// TODO Auto-generated method stub
+				changeContentFragment(index);
+			}
+		});
 	}
+
+	protected void onResume() {
+		super.onResume();
+
+		tabbar.setSelectedItem(0);
+	}
+
+	void changeContentFragment(int index) {
+		Fragment newFrag = null;
+
+		switch (index) {
+		case 0:
+			newFrag = contentFeedList;
+			break;
+		case 1:
+			newFrag = contentNoteList;
+			break;
+		case 2:
+			newFrag = contentSearchPage;
+			break;
+		case 3:
+			newFrag = contentMyProfile;
+			break;
+
+		default:
+			break;
+		}
+
+		if (newFrag == null) {
+			return;
+		}
+		getFragmentManager().beginTransaction().replace(R.id.content, newFrag).commit();
+	}
+
 }
--- src/com/example/helloworld/fragments/MainTabbarFragment.java
+++ src/com/example/helloworld/fragments/MainTabbarFragment.java
@@ -9,41 +9,67 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class MainTabbarFragment extends Fragment {
-	
+
 	View btnNew, tabFeeds, tabNotes, tabSearch, tabMe;
 	View[] tabs;
-	
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_main_tabbar, null);
-		
+
 		btnNew = view.findViewById(R.id.btn_new);
 		tabFeeds = view.findViewById(R.id.tab_feeds);
 		tabNotes = view.findViewById(R.id.tab_notes);
 		tabSearch = view.findViewById(R.id.tab_search);
 		tabMe = view.findViewById(R.id.tab_me);
-		
-		tabs = new View[] {
-				tabFeeds, tabNotes, tabSearch, tabMe
-		};
-		
-		for(final View tab : tabs){
+
+		tabs = new View[] { tabFeeds, tabNotes, tabSearch, tabMe };
+
+		for (final View tab : tabs) {
 			tab.setOnClickListener(new View.OnClickListener() {
-				
+
 				@Override
 				public void onClick(View v) {
 					onTabClicked(tab);
 				}
-			});			
+			});
 		}
-		
+
 		return view;
 	}
-	
-	void onTabClicked(View tab){
-		for(View otherTab : tabs){
-			otherTab.setSelected(otherTab == tab);
+
+	public static interface OnTabSelectedListener {
+		void onTabSelected(int index);
+	}
+
+	OnTabSelectedListener onTabSelectedListener;
+
+	public void setOnTabSelectedListener(OnTabSelectedListener onTabSelectedListener) {
+		this.onTabSelectedListener = onTabSelectedListener;
+	}
+
+	public void setSelectedItem(int index) {
+		if (index >= 0 && index < tabs.length) {
+			onTabClicked(tabs[index]);
 		}
+	}
+
+	void onTabClicked(View tab) {
+		int selectedIndex = -1;
+
+		for (int i = 0; i < tabs.length; i++) {
+			View otherTab = tabs[i];
+			if (otherTab == tab) {
+				otherTab.setSelected(otherTab == tab);
+				selectedIndex = i;
+			} else {
+				otherTab.setSelected(false);
+			}
+		}
+
+		if (onTabSelectedListener != null && selectedIndex >= 0) {
+			onTabSelectedListener.onTabSelected(selectedIndex);
+		}
+
 	}
 }
