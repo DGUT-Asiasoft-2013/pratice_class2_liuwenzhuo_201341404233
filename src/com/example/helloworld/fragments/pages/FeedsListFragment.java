0ce18b063413f3a7fef754da87c21d51c43498b8
Administrator <Administrator@7A304-0033>
2016-12-07 11:44:31 +0800

    Feed_list_fragment中添加了listview

--- src/com/example/helloworld/fragments/pages/FeedsListFragment.java
+++ src/com/example/helloworld/fragments/pages/FeedsListFragment.java
@@ -7,17 +7,62 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class FeedsListFragment extends Fragment {
 
 	private View view;
+	ListView listView;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		if (view == null) {
 			view = inflater.inflate(R.layout.fragment_page_feed_list, null);
-		}
-		return view;
 
+			listView = (ListView) view.findViewById(R.id.list);
+			listView.setAdapter(listAdapter);
+		}
+		
+		return view;
 	}
+
+	BaseAdapter listAdapter = new BaseAdapter() {
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			View view = null;
+
+			if (convertView == null) {
+
+				LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+				view = inflater.inflate(android.R.layout.simple_list_item_1, null);
+			} else {
+				view = convertView;
+			}
+			TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+			text1.setText("THIS IS ROW" + position);
+			return view;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return "";
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 20;
+		}
+	};
 }
