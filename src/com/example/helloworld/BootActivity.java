958bf6f39364d5f24f5716b280a0d9ea6ebd5fb6
Administrator <Administrator@7A304-0033>
2016-12-08 16:03:42 +0800

    画.9图和网络请求

--- src/com/example/helloworld/BootActivity.java
+++ src/com/example/helloworld/BootActivity.java
@@ -1,9 +1,16 @@
 package com.example.helloworld;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.widget.Toast;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class BootActivity extends Activity {
 
@@ -18,16 +25,56 @@ public class BootActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 
-		Handler handler = new Handler();
-		handler.postDelayed(new Runnable() {
-			private int abcd = 0;
+		// Handler handler = new Handler();
+		// handler.postDelayed(new Runnable() {
+		// private int abcd = 0;
+		//
+		// @Override
+		// public void run() {
+		// startLoginActivity();
+		// }
+		// }, 1000);
+
+		OkHttpClient client = new OkHttpClient();
+
+		Request request = new Request.Builder().url("http://172.27.0.20:8080/membercenter/api/hello").method("GET", null)
+				.build();
+
+		client.newCall(request).enqueue(new Callback() {
 
 			@Override
-			public void run() {
-				startLoginActivity();
-			}
-		}, 1000);
+			public void onResponse(Call arg0, final Response arg1) throws IOException {
+				// TODO Auto-generated method stub
+				BootActivity.this.runOnUiThread(new Runnable() {
 
+					@Override
+					public void run() {
+
+						try {
+							Toast.makeText(BootActivity.this, arg1.body().string(), Toast.LENGTH_SHORT).show();
+
+							startLoginActivity();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+
+			@Override
+			public void onFailure(Call arg0, final IOException arg1) {
+				// TODO Auto-generated method stub
+				BootActivity.this.runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						Toast.makeText(BootActivity.this, arg1.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+					}
+				});
+			}
+		});
 	}
 
 	void startLoginActivity() {
