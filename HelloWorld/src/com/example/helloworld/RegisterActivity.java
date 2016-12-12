e4dbbded450078226b1a42d0ed0f659c81379ee3
Administrator <Administrator@7A304-0034>
2016-12-12 18:37:46 +0800

    完善

---
+++ HelloWorld/src/com/example/helloworld/RegisterActivity.java
@@ -0,0 +1,40 @@
+package com.example.helloworld;
+
+import com.example.helloworld.fragments.inputcells.SimpleTextInputCellFragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+	SimpleTextInputCellFragment fragInputCellAccount;
+	SimpleTextInputCellFragment fragInputCellPassword;
+	SimpleTextInputCellFragment fragInputCellPasswordRepeat;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_register);
+
+		fragInputCellAccount = (SimpleTextInputCellFragment) getFragmentManager().findFragmentById(R.id.input_account);
+		fragInputCellPassword = (SimpleTextInputCellFragment) getFragmentManager()
+				.findFragmentById(R.id.input_password);
+		fragInputCellPasswordRepeat = (SimpleTextInputCellFragment) getFragmentManager()
+				.findFragmentById(R.id.input_passoword_repeate);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+
+		fragInputCellAccount.setLabelText("账户名");
+		fragInputCellAccount.setHintText("请输入用户名");
+		fragInputCellPassword.setLabelText("密码");
+		fragInputCellPassword.setHintText("请输入密码");
+		fragInputCellPassword.setIsPassword(true);
+		fragInputCellPasswordRepeat.setLabelText("确定密码");
+		fragInputCellPasswordRepeat.setHintText("再次输入密码");
+		fragInputCellPasswordRepeat.setIsPassword(true);
+	}
+}
