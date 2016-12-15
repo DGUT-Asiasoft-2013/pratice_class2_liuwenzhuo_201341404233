de272f9b8d80112a17a6aa316cddb60b2d5bcef6
Administrator <Administrator@7A304-0033>
2016-12-15 19:29:08 +0800

    完善AvatarView

--- src/com/example/helloworld/fragments/AvatarView.java
+++ src/com/example/helloworld/fragments/AvatarView.java
@@ -3,13 +3,14 @@ package com.example.helloworld.fragments;
 import java.io.IOException;
 
 import com.example.helloworld.entity.Server;
-import com.example.helloworld.entity.User;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Shader.TileMode;
 import android.os.Handler;
@@ -22,46 +23,54 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class AvatarView extends View {
-	
+
 	public AvatarView(Context context) {
 		super(context);
 	}
-	
+
 	public AvatarView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 	}
-	
+
 	public AvatarView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 	}
 
 	Paint paint;
-	float radius;
+	float srcWidth, srcHeight;
 	Handler mainThreadHandler = new Handler();;
-	
-	public void setBitmap(Bitmap bmp){
-		if(bmp==null){
-			return;
+
+	public void setBitmap(Bitmap bmp) {
+		if (bmp == null) {
+			paint = new Paint();
+			paint.setColor(Color.GRAY);
+			paint.setStyle(Paint.Style.STROKE);
+			paint.setStrokeWidth(1);
+			paint.setPathEffect(new DashPathEffect(new float[] { 5, 10, 15, 20 }, 0));
+			paint.setAntiAlias(true);
+		} else {
+			paint = new Paint();
+			paint.setShader(new BitmapShader(bmp, TileMode.REPEAT, TileMode.REPEAT));
+			paint.setAntiAlias(true);
+
+			srcWidth = bmp.getWidth();
+			srcHeight = bmp.getHeight();
 		}
-		paint = new Paint();
-		paint.setShader(new BitmapShader(bmp, TileMode.REPEAT, TileMode.REPEAT));
-		radius = Math.min(bmp.getWidth(), bmp.getHeight())/2;
+
 		invalidate();
 	}
-	
-	public void load(String authorAvatar){
+
+	public void load(String authorAvatar) {
+
 		OkHttpClient client = Server.getSharedClient();
-		
-		Request request = new Request.Builder()
-				.url(Server.serverAddress+authorAvatar)
-				.method("get", null)
-				.build();
-		
+
+		Request request = new Request.Builder().url(Server.serverAddress + authorAvatar).method("get", null).build();
+
 		client.newCall(request).enqueue(new Callback() {
-			
+
 			@Override
 			public void onResponse(Call arg0, Response arg1) throws IOException {
-				try{
+				try {
 					byte[] bytes = arg1.body().bytes();
 					final Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
 					mainThreadHandler.post(new Runnable() {
@@ -69,25 +78,45 @@ public class AvatarView extends View {
 							setBitmap(bmp);
 						}
 					});
-				}catch(Exception ex){
-					
+				} catch (Exception ex) {
+					mainThreadHandler.post(new Runnable() {
+						public void run() {
+							setBitmap(null);
+						}
+					});
 				}
 			}
-			
+
 			@Override
 			public void onFailure(Call arg0, IOException arg1) {
 				// TODO Auto-generated method stub
-				
+				mainThreadHandler.post(new Runnable() {
+					public void run() {
+						setBitmap(null);
+					}
+				});
 			}
 		});
 	}
-	
+
 	@Override
 	public void draw(Canvas canvas) {
 		super.draw(canvas);
-		if(paint!=null){
-			canvas.drawCircle(getWidth()/2, getHeight()/2, radius, paint);	
+		if (paint != null) {
+			canvas.save();
+
+			float dstWidth = getWidth();
+			float dstHeight = getHeight();
+
+			float scaleX = srcWidth / dstWidth;
+			float scaleY = srcHeight / dstHeight;
+
+			canvas.scale(1 / scaleX, 1 / scaleY);
+
+			canvas.drawCircle(srcWidth / 2, srcHeight / 2, Math.min(srcWidth, srcHeight) / 2, paint);
+
+			canvas.restore();
 		}
-		
+
 	}
 }
